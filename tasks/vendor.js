ba4fad65304080ae801d6c2b8c79a2a03dd792a6
io-monad <iride.monad@gmail.com>
2016-04-02 12:27:09 +0900

    Use bower version of debug module

--- tasks/vendor.js
+++ tasks/vendor.js
@@ -6,6 +6,7 @@ gulp.task("vendor", () => {
   return gulp.src([
     "node_modules/jquery/dist/jquery.min.js",
     "node_modules/lodash/lodash.min.js",
+    "bower_components/visionmedia-debug/dist/debug.js",
   ])
   .pipe(concat("vendor.js"))
   .pipe(gulp.dest(`dist/${args.vendor}/scripts`));
