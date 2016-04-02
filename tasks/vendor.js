29d9956b53778d5b4ca633596f2661dc588df31c
io-monad <iride.monad@gmail.com>
2016-04-02 15:06:09 +0900

    Add license comment for debug module in vendor.js

--- tasks/vendor.js
+++ tasks/vendor.js
@@ -1,13 +1,30 @@
 import gulp from "gulp";
+import gulpif from "gulp-if";
 import concat from "gulp-concat";
+import uglify from "gulp-uglify";
+import through from "through2";
 import args from "./lib/args";
 
 gulp.task("vendor", () => {
   return gulp.src([
     "node_modules/jquery/dist/jquery.min.js",
     "node_modules/lodash/lodash.min.js",
+    "bower_components/visionmedia-debug/Readme.md",
     "bower_components/visionmedia-debug/dist/debug.js",
   ])
+  .pipe(gulpif(f => /Readme\.md$/i.test(f.path), licenseHeader()))
+  .pipe(uglify({ preserveComments: "license" }))
   .pipe(concat("vendor.js"))
   .pipe(gulp.dest(`dist/${args.vendor}/scripts`));
 });
+
+function licenseHeader() {
+  return through.obj((file, enc, done) => {
+    const content = file.contents.toString();
+    const title = content.match(/^# *(.+)$/m)[1];
+    const license = content.match(/\n#+ *License\n((?:.|\n)+?)(?:\n#|$)/i)[1];
+    const header = `${title}\n\n${license.replace(/^\n+|\n+$/g, "")}`;
+    file.contents = new Buffer(`/*!\n${header.replace(/^/mg, " * ")}\n */`);
+    return done(null, file);
+  });
+}
