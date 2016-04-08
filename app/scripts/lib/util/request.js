ad9dc11c84334c4b6df157c592177178c5c41b1b
io-monad <iride.monad@gmail.com>
2016-04-09 04:46:56 +0900

    Fix request timeout was too short

--- app/scripts/lib/util/request.js
+++ app/scripts/lib/util/request.js
@@ -1,3 +1,5 @@
+const logger = debug("request");
+
 /**
  * Simple wrapper of XMLHttpRequest with Promise
  */
@@ -5,7 +7,7 @@ export default function request(url, options) {
   options = _.extend({
     method: "GET",
     xhr: false,
-    timeout: 30,
+    timeout: 30000,
   }, options);
 
   return new Promise((resolve, reject) => {
@@ -21,23 +23,32 @@ export default function request(url, options) {
 
     xhr.open(options.method, url, true);
     xhr.onload = () => {
+      logger(`onload on ${url}`, xhr);
       if (xhr.status !== 200) {
         rejectError(`Response is not OK: "${xhr.status} ${xhr.statusText}" for url: ${url}`);
+      } else if (xhr.responseURL && xhr.responseURL !== url) {
+        rejectError(`Response is redirected to "${xhr.responseURL}" for url: ${url}`,
+          { redirected: true, responseURL: xhr.responseURL });
       } else {
         resolve(options.xhr ? xhr : xhr.responseText);
       }
     };
     xhr.onerror = () => {
+      logger(`onerror on ${url}`, xhr);
       rejectError(`Error occurred on request for url: ${url}`);
     };
     xhr.ontimeout = () => {
+      logger(`ontimeout on ${url}`, xhr);
       rejectError(`Request timed out for url: ${url}`, { isTimeout: true });
     };
     xhr.onabort = () => {
+      logger(`onabort on ${url}`, xhr);
       rejectError(`Request has been aborted for url: ${url}`, { isAborted: true });
     };
     xhr.timeout = options.timeout;
     xhr.send(null);
+
+    logger(`Requesting ${options.method} ${url}`, xhr, options);
   });
 }
 
