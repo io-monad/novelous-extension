cb59b31dd8006b1f714a335164b7e2ef02f013ac
io-monad <iride.monad@gmail.com>
2016-04-12 06:51:02 +0900

    Improve messages for empty feeds

--- app/scripts/lib/views/messages/login-required-message.js
+++ app/scripts/lib/views/messages/login-required-message.js
@@ -1,8 +1,20 @@
+import _ from "lodash";
 import React from "react";
 import MessageBox from "../common/message-box";
+import Sites from "../../sites/sites";
 
 const LoginRequiredMessage = () => (
-  <MessageBox message="loginRequired" icon="user-times" />
+  <MessageBox message="loginRequired" icon="user-times">
+    <div className="sites">
+      {_.map(Sites, (site, name) =>
+        <a key={name} href={site.meta.url} target="_blank" className="btn btn-link">
+          <img src={`/images/sites/${name}.png`} height="16" />
+          {" "}
+          {site.meta.displayName}
+        </a>
+      )}
+    </div>
+  </MessageBox>
 );
 
 export default LoginRequiredMessage;
