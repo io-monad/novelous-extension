e9a233e39450662ff0ff36f89424f1e56a335540
io-monad <iride.monad@gmail.com>
2016-04-11 22:48:31 +0900

    Split OptionsForm into component

---
+++ app/scripts/lib/views/options/options-form.js
@@ -0,0 +1,29 @@
+import React, { PropTypes } from "react";
+import Form from "react-jsonschema-form";
+import Str from "../common/str";
+import CustomSchemaField from "./custom-schema-field";
+
+const FormFields = {
+  SchemaField: CustomSchemaField,
+};
+
+const OptionsView = (props) => (
+  <div className="options-form">
+    <Form {...props} fields={FormFields}>
+      <div className="options-form__actions">
+        <button type="submit" className="options-form__save-button btn btn-primary">
+          <Str name="saveOptions" />
+        </button>
+      </div>
+    </Form>
+  </div>
+);
+
+OptionsView.propTypes = {
+  schema: PropTypes.object.isRequired,
+  uiSchema: PropTypes.object,
+  formData: PropTypes.object.isRequired,
+  onSubmit: PropTypes.function,
+};
+
+export default OptionsView;
--- app/scripts/lib/views/options/options-view.js
+++ app/scripts/lib/views/options/options-view.js
@@ -1,11 +1,5 @@
 import React, { PropTypes } from "react";
-import Form from "react-jsonschema-form";
-import Str from "../common/str";
-import CustomSchemaField from "./custom-schema-field";
-
-const FormFields = {
-  SchemaField: CustomSchemaField,
-};
+import OptionsForm from "./options-form";
 
 export default class OptionsView extends React.Component {
   constructor(props) {
@@ -18,22 +12,7 @@ export default class OptionsView extends React.Component {
   render() {
     return (
       <div className="options-view">
-        <div className="options-form">
-          <Form
-            {...this.props}
-            fields={FormFields}
-            onSubmit={this.handleFormSubmit}
-          >
-            <div className="options-form__actions">
-              <button
-                type="submit"
-                className="options-form__save-button btn btn-primary"
-              >
-                <Str name="saveOptions" />
-              </button>
-            </div>
-          </Form>
-        </div>
+        <OptionsForm {...this.props} onSubmit={this.handleFormSubmit} />
       </div>
     );
   }
